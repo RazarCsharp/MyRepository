0f83a4630c97b94bc60ca4abab2178ba46132f06
RazarCsharp <razar1992@gmail.com>
2022-08-12 03:17:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp555555/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp555555
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
